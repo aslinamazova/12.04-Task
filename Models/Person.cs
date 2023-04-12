c2104afc38aff18def9b829e8fff5392c4f456c2
aslinamazova <76001yw@code.edu.az>
2023-04-12 12:27:37 +0400

    first commit

---
+++ Models/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace _12._04Task.Models
+{
+	public class Person
+	{
+        public string Surname { get; set; }
+        public double BirthDay { get; set; }
+        private string _name;
+        private int _age;
+
+        public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if (value.Length>2)
+				{
+					_name=value;
+                    return;
+                }
+                Console.WriteLine("Give me another name");
+            }
+		}
+
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 16)
+                {
+                    _age = value;
+                    return;
+                }
+                Console.WriteLine("Give me another age");
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using _12._04Task.Models;
+
+namespace _12._04Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Asli",
+                Surname="Namazova",
+                Age=20,
+                BirthDay =08.2003
+
+            };
+
+            //var time = new DateTime(18, 08, 2003);
+            //person.BirthDay = time;
+            
+        }
+    }
+}
+
